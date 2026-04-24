3e99769083931820609bfe54541f193c07c04e4d
Toi-sai-roi <letung03102005@gmail.com>
2026-04-24 21:28:06 +0700

    Update ShoeStore - Add categories, products, payment method

--- tests/newFile.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('check homepage loads', async ({ page }) => {
-    await page.goto('http://localhost:7165');
-    const title = await page.title();
-    console.log('Title:', title);
-    expect(title).toBeTruthy();
-});
